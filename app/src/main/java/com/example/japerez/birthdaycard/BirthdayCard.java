15727896295c1227dc88960d9db02d73bc95bd2c
Javier Perez <34148619+javtech95@users.noreply.github.com>
2018-08-29 10:45:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/japerez/birthdaycard/BirthdayCard.java
@@ -0,0 +1,13 @@
+package com.example.japerez.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BirthdayCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bithday_card);
+    }
+}
